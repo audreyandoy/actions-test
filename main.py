14c14a0ee71230d71618a66d7383a6241ad9c85a
Audrey Andoy <audrey@Audreys-MacBook-Air.local>
2020-11-29 16:19:06 -0800

    test 3

--- main.py
+++ main.py
@@ -2,3 +2,4 @@
 # test actions
 # testing PR labeler
 # testin PR labeler pt. 2, fixed spelling error
+# using "https://selleo.com/til/posts/iumvmfrkhn-automate-pull-request-labels-via-github-actions" for guidance

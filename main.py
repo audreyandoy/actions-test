2b7da57ac52f1ee4459c38fbc9c5b00d66fde4d4
Audrey Andoy <audrey@Audreys-MacBook-Air.local>
2020-11-29 18:22:02 -0800

    let's try again

--- main.py
+++ main.py
@@ -3,4 +3,5 @@
 # testing PR labeler
 # testin PR labeler pt. 2, fixed spelling error
 # using "https://selleo.com/til/posts/iumvmfrkhn-automate-pull-req
-# action didn't run?
+# action didn't run?d
+# changed to pull_request_target ?
